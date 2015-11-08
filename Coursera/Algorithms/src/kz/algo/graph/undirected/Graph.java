c4497f51cec290cba6348f138567bb697f87953a
mukhan85 <mukhan.m@gmail.com>
2015-11-08 16:53:37 +0000

    updated Graph implementation

--- Coursera/Algorithms/src/kz/algo/graph/undirected/Graph.java
+++ Coursera/Algorithms/src/kz/algo/graph/undirected/Graph.java
@@ -4,6 +4,11 @@ package kz.algo.graph.undirected;
  * Created by mukhan on 08/11/15.
  */
 public class Graph {
+    private final int numVerts;
+
+    public Graph(int numVerts) {
+        this.numVerts = numVerts;
+    }
 
 
 }
